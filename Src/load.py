f2068081f10470e048009ec818a977ee26191f7d
“z” <u6015364@anu.edu.au>
2018-09-08 00:41:43 +1000

    Update load and train

--- Src/load.py
+++ Src/load.py
@@ -1,39 +1,18 @@
 import os
-import cv2
 import numpy as np
-import tqdm
-from layer import *
-from network import *
 
 def load(dir_='../data/npy'):
-    x_train = np.load(os.path.join(dir_, 'x_train.npy'))
-    x_test = np.load(os.path.join(dir_, 'x_test.npy'))
-    return x_train, x_test
+    files = os.listdir(dir_)
+    train_files = [x for x in files if "train" in x]
+    test_files = [x for x in files if "test" in x]
+    
+    x_train = []
+    x_test = []
+    
+    for f in train_files:
+        x_train.append(np.load(os.path.join(dir_, f)))
+    for f in test_files:
+        x_test.append(np.load(os.path.join(dir_, f)))
+    
+    return np.vstack(x_train), np.vstack(x_test)
 
-# Hyperparameters
-IMAGE_SIZE2 = 160
-IMAGE_SIZE1 = 256
-LOCAL_SIZE = 64
-HOLE_MIN = 24
-HOLE_MAX = 48
-LEARNING_RATE = 1e-3
-BATCH_SIZE = 16
-PRETRAIN_EPOCH = 100
-
-x = tf.placeholder(tf.float32, [BATCH_SIZE, IMAGE_SIZE1, IMAGE_SIZE2, 3])
-mask = tf.placeholder(tf.float32, [BATCH_SIZE, IMAGE_SIZE1, IMAGE_SIZE2, 1])
-local_x = tf.placeholder(tf.float32, [BATCH_SIZE, LOCAL_SIZE, LOCAL_SIZE, 3])
-global_completion = tf.placeholder(tf.float32, [BATCH_SIZE, IMAGE_SIZE1, IMAGE_SIZE2, 3])
-local_completion = tf.placeholder(tf.float32, [BATCH_SIZE, LOCAL_SIZE, LOCAL_SIZE, 3])
-is_training = tf.placeholder(tf.bool, [])
-
-model = Network(x, mask, local_x, global_completion, local_completion, is_training, batch_size=BATCH_SIZE)
-
-global_step = tf.Variable(0, name='global_step', trainable=False)
-epoch = tf.Variable(0, name='epoch', trainable=False)
-
-opt = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-
-g_train_op = opt.minimize(model.g_loss, global_step=global_step, var_list=model.g_variables)
-
-d_train_op = opt.minimize(model.d_loss, global_step=global_step, var_list=model.d_variables)
