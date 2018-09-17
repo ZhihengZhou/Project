458222628ffd7ddc15946dc0a10c9c575a2ba90d
“z” <u6015364@anu.edu.au>
2018-09-17 18:12:50 +1000

    Update load

--- Src/load.py
+++ Src/load.py
@@ -15,3 +15,16 @@ def load(dir_='../Data/UIdata/npy/'):
         x_test.append(np.load(os.path.join(dir_, f)))
     
     return np.vstack(x_train), np.vstack(x_test)
+
+def load_test(dir_='../../Data/UIdata/npy/'):
+    files = os.listdir(dir_)
+    
+    test_files = [x for x in files if "test" in x]
+    
+    x_test = []
+    
+    for f in test_files:
+        x_test.append(np.load(os.path.join(dir_, f)))
+    
+    return np.vstack(x_test)
+
